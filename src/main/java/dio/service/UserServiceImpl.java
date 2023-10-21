90b0baaf739465db48865d59d343aa54827e0104
lobodavi <lobodavi@hotmail.com>
2023-10-21 10:33:54 -0300

    v1.0

---
+++ src/main/java/dio/service/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package dio.service;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.stereotype.Service;
+
+import dio.domain.model.User;
+import dio.domain.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    public User create(User userToCreate) {
+        if (userRepository.existsById(userToCreate.getId()) && userToCreate.getId() != null) {
+            throw new IllegalArgumentException("This User ID already exists.");
+        }
+        return userRepository.save(userToCreate);
+    }
+}
